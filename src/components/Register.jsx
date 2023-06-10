dd69da49f8ecd750e3690923c39223e1621d2ba5
kjacyno <jacynko@gmail.com>
2023-06-10 14:16:31 +0200

    added react hook form to register-page

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,23 +1,33 @@
 /* eslint-disable react/prop-types */
 import {Container} from '@mui/material';
-import {useState} from 'react';
-import {Link} from 'react-router-dom';
+import {useForm} from "react-hook-form";
+// import {useState} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
 import Header from '../components/Header.jsx';
 import {createNewUser} from '../firebase/firebaseAuth.js';
 
 export default function Register({user, setUser}) {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
 
+    const {register, handleSubmit, getValues, formState: {errors}} = useForm({
+        defaultValues: {
+            name: '',
+            email: '',
+            msg: '',
+        }
+    });
+    const navigate = useNavigate();
 
     async function handleNewUser(event) {
         event.preventDefault();
+        const email = getValues('email');
+        const password = getValues('password')
         await createNewUser(
             {
                 displayName: email
             },
             setUser, user, email, password
         );
+        navigate('/')
 
     }
 
@@ -28,20 +38,34 @@ export default function Register({user, setUser}) {
                 <h2>Załóż konto</h2>
                 <img src="/src/assets/Decoration.svg" alt="decoration"/>
                 <form id='register-form'
-                      onSubmit={handleNewUser}
+                      onSubmit={handleSubmit(handleNewUser)}
                       >
                     <label htmlFor="email">Email</label>
                     <input type="email"
-                           value={email}
+                           {...register('email', {
+                               required: 'Podany e-mail jest nieprawidłowy',
+                               pattern: {
+                                   value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                                   message: 'Podany e-mail jest nieprawidłowy'
+                               }
+                           })}
                            id="email"
-                           placeholder="e-mail"
-                           onChange={(event) => setEmail(event.target.value)}/>
+                           className={errors.email?.message ? 'error' : ''}
+                    />
+                    <p className='error-message'>{errors.email?.message}</p>
                     <label htmlFor="password">Hasło</label>
                     <input type="password"
-                           id='password'
-                           value={password}
-                           placeholder='hasło'
-                           onChange={(event) => setPassword(event.target.value)}/>
+                           {...register('password', {required: 'Podaj hasło'})}
+                           className={errors.password?.message ? 'error' : ''}
+                           id='password'/>
+                    <p className='error-message'>{errors.password?.message}</p>
+
+                    <label htmlFor="password">Powtórz hasło</label>
+                    <input type="password"
+                           {...register('confirm', {required: 'Potwierdź hasło'})}
+                           id='confirm'/>
+                    <p className='error-message'>{errors.confirm?.message}</p>
+
                 </form>
                 <div className='login-menu'>
                 <Link to={'/login'} className='login-btn'>Zaloguj się</Link>
